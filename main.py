30d3abd467f25861bb447cabc246d3215a0f3601
Fantah Kabba <fkelk1@gmail.com>
2019-07-22 12:07:17 -0700

    created main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+from google.appenginge.ext nbd
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)

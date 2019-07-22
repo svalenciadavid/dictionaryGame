7cf0dc7b77aeb08d1f1ccc9b5b18595abc8bbc21
David Valencia <svalenciadavid@gmail.com>
2019-07-22 14:27:01 -0500

    added users library

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from google.appenginge.ext nbd
 import jinja2
 import webapp2
 
+from google.appengine.api import users
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],

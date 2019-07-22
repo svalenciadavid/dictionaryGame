8c907aa52db7d6ef9745ff76b7cf0752ac216be6
christian <cdb89@cornell.edu>
2019-07-22 12:38:04 -0700

    added Google Sign-In

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 
-from google.appenginge.ext import nbd
+from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
 
 from google.appengine.api import users
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/loginPage/login.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,9 +19,9 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/login', LoginPage),
-    ('/home', HomePage),
-    ('/host', HostPage),
-    ('/player', PlayerPage)
+    ('/', MainPage)
+    #('/login', LoginPage),
+    #('/home', HomePage),
+    #('/host', HostPage),
+    #('/player', PlayerPage)
 ], debug=True)

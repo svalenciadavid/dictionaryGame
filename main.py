319b037710048261f09b3e699e9e183895dfe0b1
Neeva Sethi <sethineeva@gmail.com>
2019-07-22 16:40:54 -0500

    Creating a Login/logout with home page

--- main.py
+++ main.py
@@ -13,10 +13,29 @@ def root_parent():
 
 class LoginPage(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENVIRONMENT.get_template('templates/loginPage/login.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url('/home'),
+          'logout_url': users.create_logout_url('/'),
+        }
         template = JINJA_ENVIRONMENT.get_template('templates/loginPage/login.html')
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render())
+        self.response.write(template.render(data))
 
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENVIRONMENT.get_template('templates/homePage/homePage.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url('/home'),
+          'logout_url': users.create_logout_url('/'),
+        }
+        template = JINJA_ENVIRONMENT.get_template('templates/homePage/homePage.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render(data))
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -24,8 +43,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 app = webapp2.WSGIApplication([
-    ('/', LoginPage)
-    #('/home', HomePage),
+    ('/', LoginPage),
+    ('/home', HomePage),
     #('/host', HostPage),
     #('/player', PlayerPage)
 ], debug=True)

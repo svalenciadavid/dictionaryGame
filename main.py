c0c60192e6355f721ab40fd99c82d61d85c6089c
David Valencia <svalenciadavid@gmail.com>
2019-07-23 09:28:58 -0500

    removed comment prints

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class LoginPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        print user
         if user:
-            print "SEEEE"
             template = JINJA_ENVIRONMENT.get_template('templates/homePage/homePage.html')
         elif not user:
             template = JINJA_ENVIRONMENT.get_template('templates/loginPage/login.html')
